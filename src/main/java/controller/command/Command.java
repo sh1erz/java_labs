3cd15e982f2ae3a4ad81ee21abedb027c8ac45af
Karyna Rebrishcheva <karyna_rebrishcheva@epam.com>
2021-12-07 18:29:56 +0200

    tomcat

--- src/main/java/controller/command/Command.java
+++ src/main/java/controller/command/Command.java
@@ -1,7 +1,8 @@
 package controller.command;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public interface Command {
     String execute(HttpServletRequest req, HttpServletResponse resp);
