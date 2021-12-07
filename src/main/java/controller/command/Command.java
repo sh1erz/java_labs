5a931a0727221b01be460ac761865f99157f92e5
Karyna Rebrishcheva <karyna_rebrishcheva@epam.com>
2021-12-07 17:21:45 +0200

    completion -> performer

--- src/main/java/controller/command/Command.java
+++ src/main/java/controller/command/Command.java
@@ -1,4 +1,8 @@
 package controller.command;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public interface Command {
+    String execute(HttpServletRequest req, HttpServletResponse resp);
 }
--- src/main/java/model/dao/interfaces/MedcardRecordDao.java
+++ src/main/java/model/dao/interfaces/MedcardRecordDao.java
@@ -2,10 +2,10 @@ package model.dao.interfaces;
 
 import domain.MedcardRecord;
 
-import java.sql.ResultSet;
 import java.util.List;
 
 public interface MedcardRecordDao extends Dao<MedcardRecord, Integer> {
     List<MedcardRecord> getPatientMedcardRecords(int patientId);
+
     boolean setPerformer(int medcardId, String performer);
 }
